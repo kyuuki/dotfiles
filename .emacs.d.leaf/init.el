d4d3008eca2ff2492f404f6cd137ae0d34a89fda
kyuuki <kyuuki.japan@gmail.com>
2021-02-07 19:02:45 +0900

    Not making backupfiles for Emacs

--- .emacs.d.leaf/init.el
+++ .emacs.d.leaf/init.el
@@ -22,6 +22,12 @@
 ;; http://yohshiy.blog.fc2.com/blog-entry-273.html
 (prefer-coding-system 'utf-8-unix)
 
+;;;; 表示
+(if window-system
+  (tool-bar-mode 0)
+  (menu-bar-mode -1)
+)
+
 ;;;; leaf
 
 ;; <leaf-install-code>
@@ -48,8 +54,16 @@
     (leaf-keywords-init)))
 ;; </leaf-install-code>
 
-(leaf cus-start
-  :custom
-  (if (window-system) (menu-bar-mode . nil)))
-
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(blackout el-get hydra leaf-keywords leaf)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
