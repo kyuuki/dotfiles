f868239c44801ef0d98a10c8327c529d75059edc
kyuuki <kyuuki.japan@gmail.com>
2021-02-10 14:23:25 +0000

    Add East Asian Ambiguous Width setting to Emacs

--- .emacs.d.leaf/init.el
+++ .emacs.d.leaf/init.el
@@ -61,16 +61,14 @@
     (leaf-keywords-init)))
 ;; </leaf-install-code>
 
+;;;; East Asian Ambiguous 問題
+;; https://github.com/hamano/locale-eaw
+;; https://uwabami.github.io/cc-env/Emacs.html ; これは矢印がダメだった
+(leaf eaw
+  :el-get hamano/locale-eaw
+  :require t
+  :config
+  (eaw-fullwidth)
+  )
+
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(blackout el-get hydra leaf-keywords leaf)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
